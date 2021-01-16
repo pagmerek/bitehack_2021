430b8bf00d49db67504ed4ec415b2ecab99613ca
229869 Durys Bartosz <229869@edu.p.lodz.pl>
2021-01-16 15:49:38 +0100

    update wolfram

--- researcher_app/api/wolfram.py
+++ researcher_app/api/wolfram.py
@@ -6,11 +6,11 @@ client = wolframalpha.Client(app_id)
 def wolframGetRes(query):
     res = client.query(query)
 
-    print(wolframCheckRes(res))
-    print(next(res.results).text)
+    if wolframCheckRes(res):
+        return next(res.results).text
+    else:
+        return ""
 
 def wolframCheckRes(res):
     status = res['@success'] and not(res['@error'])
     return status
-
-wolframGetRes("cat")
