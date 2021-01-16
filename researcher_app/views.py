022abf7af0b2a8b35b8dee453016fbb8d601b8ff
pagmerek <p.gmerek@student.uj.edu.pl>
2021-01-16 16:19:23 +0100

    success

--- researcher_app/views.py
+++ researcher_app/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from researcher_app.forms import SearchForm
+from researcher_app.api.wiki import query
 # Create your views here.
 def index(request):
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
             text = form['search_text'].value()
-            print(text)
+            print(query(text))
+            return render(request, 'index.html', {'form':form, 'success':True})
     else:
         form = SearchForm()
 
