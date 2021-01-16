0f69be163874a17aaed604449fc18c77acf12fbb
229869 Durys Bartosz <229869@edu.p.lodz.pl>
2021-01-16 15:46:14 +0100

    simple wolfram api functions

--- researcher_app/api/wolfram.py
+++ researcher_app/api/wolfram.py
@@ -1,1 +1,16 @@
 import wolframalpha
+
+app_id = 'W3L5WX-P6EAVKL8XV'
+client = wolframalpha.Client(app_id)
+
+def wolframGetRes(query):
+    res = client.query(query)
+
+    print(wolframCheckRes(res))
+    print(next(res.results).text)
+
+def wolframCheckRes(res):
+    status = res['@success'] and not(res['@error'])
+    return status
+
+wolframGetRes("cat")
