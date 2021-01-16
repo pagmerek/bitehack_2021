13b7c98bc9d788ee2fd61d487d15d9a88964b814
229869 Durys Bartosz <229869@edu.p.lodz.pl>
2021-01-16 17:29:12 +0100

    layout for answers

---
+++ researcher_app/static/colorGen.js
@@ -0,0 +1,14 @@
+answersColors = ["#FBF55A", "#A1FB5A", "#B45AFB", "#5ACBFB", "#FBC45A", "#5A60FB", "#FB5AD7", "#FB5A5A"]
+
+function setColor(divAnswer) {
+    console.log(divAnswer);
+    divAnswer.style.backgroundColor = answersColors[Math.floor(Math.random() * answersColors.length)];
+}
+
+const answers = document.getElementsByClassName("answer");
+
+if(answers) {
+    for (let i = 0; i < answers.length; i++) {
+        setColor(answers[i]);
+    }
+}
