c11163747503a76f555e089422d2e4088f2fe0e9
pagmerek <p.gmerek@student.uj.edu.pl>
2021-01-16 23:47:04 +0100

    forms for checkboxes was a bad idea"

--- researcher_app/forms.py
+++ researcher_app/forms.py
@@ -8,9 +8,3 @@ class SearchForm(forms.Form):
     def __str__(self):
         return str(self.search_text)
 
-
-class SelectForm(forms.Form):
-    wikipedia = forms.BooleanField()
-    stack= forms.BooleanField()
-    wolfram = forms.BooleanField()
-    google = forms.BooleanField()
