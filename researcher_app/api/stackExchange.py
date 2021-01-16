7c9a248efa68e83dc4dd06e896b8a251c06f72b9
pagmerek <p.gmerek@student.uj.edu.pl>
2021-01-16 20:23:49 +0100

    stack exchange api

---
+++ researcher_app/api/stackExchange.py
@@ -0,0 +1,10 @@
+import stackexchange as ste
+
+
+def query(res):
+    so = ste.Site(ste.StackOverflow)
+    stack_table = []
+    for j, i in enumerate(so.search(intitle=res,sort='votes')):
+        if j > 5: break
+        stack_table.append([i.owner.profile_image, i.owner.display_name, i.score, i.title, i.link])
+    return stack_table
