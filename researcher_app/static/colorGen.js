5fee7f72d4dc756f867abdd12d10456b83025268
229869 Durys Bartosz <229869@edu.p.lodz.pl>
2021-01-17 01:15:07 +0100

    add <a> on images

--- researcher_app/static/colorGen.js
+++ researcher_app/static/colorGen.js
@@ -1,4 +1,4 @@
-answersColors = ["#CC6666", "#CEAA78", "#FF9966", "99CC99"]
+answersColors = ["#CC6666", "#CEAA78", "#FF9966", "#99CC99"]
 
 function setColor(divAnswer) {
     divAnswer.style.backgroundColor = answersColors[Math.floor(Math.random() * answersColors.length)];
